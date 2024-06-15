cce6e8bbf2fe7c66d8647b94373cd63de94cad27
jafar <se.hasanjafar@gmail.com>
2024-06-15 13:03:14 +0530

    14 june

---
+++ JUNE 2024/945_Minimum_Increment_to_Make_Array_Unique.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    // TC;O(NlogN) SC:O(1)
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int cnt = nums[0];
+        int move = 0;
+        for (int i = 1; i < nums.length; i++) {
+            cnt++;
+            if (nums[i] < cnt) {
+                move += Math.abs(nums[i] - cnt);
+            } else {
+                cnt = nums[i];
+            }
+        }
+        return move;
+
+    }
+}
