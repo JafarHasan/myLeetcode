6263c1c8f7a4f1c24e02655d78844578964103c2
jafar <se.hasanjafar@gmail.com>
2024-06-16 09:41:34 +0530

    Question16

---
+++ JUNE 2024/330_Patching_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long sum = 0; // sum may be out of range for int
+        int cnt = 0;
+        int i = 0;
+
+        while (sum < n) {
+            if (i < nums.length && nums[i] <= sum + 1) {
+                sum += nums[i];
+                i++;
+            } else {
+                sum += sum + 1; // Add sum + 1 to `sum`, and now we need one patch
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
