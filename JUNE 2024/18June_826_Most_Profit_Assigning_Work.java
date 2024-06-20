83582d9bbf92762e3d594ac5432042bc58d7f2d9
Jafar Hasan <se.hasanjafar@gmail.com>
2024-06-20 13:36:41 +0530

    binary search

--- JUNE 2024/18June_826_Most_Profit_Assigning_Work.java
+++ JUNE 2024/18June_826_Most_Profit_Assigning_Work.java
@@ -17,4 +17,39 @@ class Solution {
         }
         return sum;
     }
+
+    // Optimization
+    // TC:O(N+M+maxwrkr+1)=>O(N) SC:O(jobs.length())
+    public int maxProfitAssignment(int[] diff, int[] profit, int[] wkr) {
+        int max = 0;
+        int n = diff.length;
+        int m = wkr.length;
+
+        // find the maximum workr (maximum difficulty level)
+        int maxwrkr = Integer.MIN_VALUE;
+        for (int ele : wkr) {
+            maxwrkr = Math.max(maxwrkr, ele);
+        }
+
+        int jobs[] = new int[maxwrkr + 1];
+        // fill the jobs array with difficulty profit
+        for (int i = 0; i < n; i++) {
+            int ele = diff[i];
+            if (ele <= maxwrkr) // avoiding greater difficulty than maxwrkr
+                jobs[ele] = Math.max(jobs[ele], profit[i]);
+        }
+
+        // fill the remaining space (currently 0)with max(jobs[i],jobs[i-1])
+        for (int i = 1; i < jobs.length; i++) {
+            jobs[i] = Math.max(jobs[i], jobs[i - 1]);
+        }
+
+        // itearte over wrkr array and check difficulty in jobs and calculate sum
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            int idx = wkr[i];
+            sum += jobs[idx];
+        }
+        return sum;
+    }
 }
