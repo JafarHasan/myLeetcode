09ee886245f50d005eadee3bcd0989a36ee1bbd9
Jafar Hasan <se.hasanjafar@gmail.com>
2024-06-22 10:44:56 +0530

    sliding window and HashMap

---
+++ JUNE 2024/22June_1248_Count_Number_of_Nice_Subarrays.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    // TC:O(N) SC:O(N) HashMap Sliding Window Solution
+    public int numberOfSubarrays(int[] nums, int k) {
+        int cntNice = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);// we hv seen 0 sum 1 time
+        int no = 0;
+        for (int ele : nums) {
+            // considering every odd no as 1 and even no as 0
+            // and trying to find cnt of subarray whose sum equals to k
+            if (ele % 2 == 1) {
+                no = 1;
+            } else {
+                no = 0;
+            }
+            sum += no;
+            if (map.containsKey(sum - k)) {
+                cntNice += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return cntNice;
+    }
+}
