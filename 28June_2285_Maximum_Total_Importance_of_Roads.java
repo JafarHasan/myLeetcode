f9d034980c2d4fc6dcb9e419a91b83d59041a161
Jafar Hasan <se.hasanjafar@gmail.com>
2024-06-28 09:36:03 +0530

    Indegree Array approach

---
+++ 28June_2285_Maximum_Total_Importance_of_Roads.java
@@ -0,0 +1,20 @@
+class Solution {
+    // TC:O(NlogN) SC:O(N)
+    public long maximumImportance(int n, int[][] roads) {
+        long indegree[] = new long[n]; // to store the indegree of each nodes
+        for (int[] edge : roads) {
+            int src = edge[0];
+            int dest = edge[1];
+            indegree[src]++;
+            indegree[dest]++;
+        }
+        Arrays.sort(indegree);
+        long ans = 0l;
+        long number = 1;
+        for (long val : indegree) {
+            ans += val * number;
+            number++;
+        }
+        return ans;
+    }
+}
