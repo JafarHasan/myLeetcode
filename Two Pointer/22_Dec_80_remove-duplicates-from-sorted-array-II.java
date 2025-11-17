3d2c5f957cde88f40a703f071606ffd1d9c4f9f8
Jafar Hasan <se.hasanjafar@gmail.com>
2025-11-17 15:30:17 +0530

    two pointer Solved

--- Two Pointer/22_Dec_80_remove-duplicates-from-sorted-array-II.java
+++ Two Pointer/22_Dec_80_remove-duplicates-from-sorted-array-II.java
@@ -1,4 +1,37 @@
 
+class Solution_best {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) return nums.length; // if size ≤ 2, no need to modify
+
+        int k = 2;  // start from 3rd position (since first two are always allowed)
+
+        for (int i = 2; i < nums.length; i++) {
+            // Compare current num with the element two places before in the valid portion
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
+class Solution_One {
+    public int removeDuplicates(int[] nums) {
+        int j = 0, k = 1;
+        for (int i = 2; i < nums.length; i++) {
+            //already same
+            if (nums[j] == nums[k] && nums[k] == nums[i])
+                continue;
+
+            j = k;
+            k++;
+            nums[k] = nums[i];
+            
+        }
+        return k + 1;
+    }
+}
 class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 1;
