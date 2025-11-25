cbc5e14538eff763540f8d99269bae16303b7ccf
Jafar Hasan <se.hasanjafar@gmail.com>
2025-11-25 21:01:48 +0530

    Interview Questions

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,30 +1,18 @@
 
-public class Main {
+class main {
 
     public static void main(String[] args) {
-        String s = "Hello ZAFAR Hasan";
-        System.out.println("String is:" + s);
-        // s = s.replace('a', '@');
-        // System.out.println("String is:" + s);
-        // s = s.replace("Hasan","Zafar");
-        // System.out.println("String is:" + s);
-        String s1 = "Hello";
-        String s2 = "hello";
-        int val = s1.compareTo(s2);
-        if (val > 0) {
-            System.out.println(s1 + " is Bigger");
-        } else if (val < 0) {
-            System.out.println(s2 + " is Bigger");
-        } else {
-            System.out.println(s1 + "=" + s2);
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("* ");
+            }
+            int start=(i%2==0)?1: 0;
+            for (int j = 1; j <= i; j++) {
+               System.out.print(start+" ");
+               start=1-start;
+            }
+            System.out.println();
         }
-
-        String s3 = "12345";
-        System.out.println(Integer.parseInt(s3));
-
-        StringBuilder s4 = new StringBuilder("Hello world");
-        System.out.println("s4=" + s4.insert(0, 9));
-        System.out.println("s4=" + s4.deleteCharAt(0));
-
     }
 }
